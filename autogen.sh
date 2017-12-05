4f971f89ff2b60930c9cd05903542b354fc2ce5f
ocrespo <o.crespo@worldcastsystems.com>
2017-12-05 13:00:41 +0000

    Updated autogen to newer version

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -x
-AUTOMAKE=${AUTOMAKE:-automake-1.9} ACLOCAL=${ACLOCAL:-aclocal-1.9}
+AUTOMAKE=${AUTOMAKE:-automake-1.14} ACLOCAL=${ACLOCAL:-aclocal-1.14}
 export AUTOMAKE ACLOCAL
 ${AUTORECONF:-autoreconf} -i
 find . \( -name 'run*' -o -name '*.sh' \) -a -type f | xargs chmod +x
